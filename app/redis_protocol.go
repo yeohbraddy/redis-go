4e980d55aedbcee687dd963c1f3fcbed50ccc5c2
Braddy Yeoh <braddy.yeoh@ucdconnect.ie>
2022-08-29 17:14:07 +0100

    code cleanup

--- app/redis_protocol.go
+++ app/redis_protocol.go
@@ -1,36 +1,36 @@
 package main
 
-import {
+import (
 	"bufio"
 	"fmt"
 	"io"
 	"strconv"
-}
+)
 
 type Type byte
 
-const {
+const (
 	SimpleString Type = '*'
-	BulkString Type = '$'
-	Array Type = '*'
-}
+	BulkString   Type = '$'
+	Array        Type = '*'
+)
 
 type Value struct {
-	typ Type 
+	typ   Type
 	bytes []byte
-	array []value
+	array []Value
 }
 
 func (v Value) String() string {
-if v.typ == BulkString || v.typ == SimpleString {
-	return string(v.bytes)
+	if v.typ == BulkString || v.typ == SimpleString {
+		return string(v.bytes)
+	}
+
+	return ""
+
 }
 
-return ""
-
-}
-
-func (v Value) Array() []Value{
+func (v Value) Array() []Value {
 	if v.typ == Array {
 		return v.array
 	}
@@ -45,7 +45,7 @@ func DecodeRESP(byteStream *bufio.Reader) (Value, error) {
 	}
 
 	switch string(dataTypeByte) {
-	case "*":
+	case "+":
 		return decodeSimpleString(byteStream)
 	case "$":
 		return decodeBulkString(byteStream)
@@ -57,82 +57,82 @@ func DecodeRESP(byteStream *bufio.Reader) (Value, error) {
 }
 
 func decodeSimpleString(byteStream *bufio.Reader) (Value, error) {
-		readBytes, err := readUntilCRLF(byteStream)
+	readBytes, err := readUntilCRLF(byteStream)
+	if err != nil {
+		return Value{}, err
+	}
+
+	return Value{
+		typ:   SimpleString,
+		bytes: readBytes,
+	}, nil
+}
+
+func decodeBulkString(byteStream *bufio.Reader) (Value, error) {
+	readBytesForCount, err := readUntilCRLF(byteStream)
+	if err != nil {
+		return Value{}, fmt.Errorf("failed to read bulk string length: %s", err)
+	}
+
+	count, err := strconv.Atoi(string(readBytesForCount))
+	if err != nil {
+		return Value{}, fmt.Errorf("failed to parse bulk string length: %s", err)
+	}
+
+	readBytes := make([]byte, count+2)
+
+	if _, err := io.ReadFull(byteStream, readBytes); err != nil {
+		return Value{}, fmt.Errorf("failed to read bulk string contents: %s", err)
+	}
+
+	return Value{
+		typ:   BulkString,
+		bytes: readBytes[:count],
+	}, nil
+}
+
+func decodeArray(byteStream *bufio.Reader) (Value, error) {
+	readBytesForCount, err := readUntilCRLF(byteStream)
+	if err != nil {
+		return Value{}, fmt.Errorf("failed to read bulk string length: %s", err)
+	}
+
+	count, err := strconv.Atoi(string(readBytesForCount))
+	if err != nil {
+		return Value{}, fmt.Errorf("failed to parse bulk string length: %s", err)
+	}
+
+	array := []Value{}
+
+	for i := 1; i <= count; i++ {
+		value, err := DecodeRESP(byteStream)
 		if err != nil {
 			return Value{}, err
 		}
-	
-		return Value{
-			typ:   SimpleString,
-			bytes: readBytes,
-		}, nil
+
+		array = append(array, value)
 	}
-	
-	func decodeBulkString(byteStream *bufio.Reader) (Value, error) {
-		readBytesForCount, err := readUntilCRLF(byteStream)
+
+	return Value{
+		typ:   Array,
+		array: array,
+	}, nil
+}
+
+func readUntilCRLF(byteStream *bufio.Reader) ([]byte, error) {
+	readBytes := []byte{}
+
+	for {
+		b, err := byteStream.ReadBytes('\n')
 		if err != nil {
-			return Value{}, fmt.Errorf("failed to read bulk string length: %s", err)
+			return nil, err
 		}
-	
-		count, err := strconv.Atoi(string(readBytesForCount))
-		if err != nil {
-			return Value{}, fmt.Errorf("failed to parse bulk string length: %s", err)
+
+		readBytes = append(readBytes, b...)
+		if len(readBytes) >= 2 && readBytes[len(readBytes)-2] == '\r' {
+			break
 		}
-	
-		readBytes := make([]byte, count2)
-	
-		if _, err := io.ReadFull(byteStream, readBytes); err != nil {
-			return Value{}, fmt.Errorf("failed to read bulk string contents: %s", err)
-		}
-	
-		return Value{
-			typ:   BulkString,
-			bytes: readBytes[:count],
-		}, nil
 	}
-	
-	func decodeArray(byteStream *bufio.Reader) (Value, error) {
-		readBytesForCount, err := readUntilCRLF(byteStream)
-		if err != nil {
-			return Value{}, fmt.Errorf("failed to read bulk string length: %s", err)
-		}
-	
-		count, err := strconv.Atoi(string(readBytesForCount))
-		if err != nil {
-			return Value{}, fmt.Errorf("failed to parse bulk string length: %s", err)
-		}
-	
-		array := []Value{}
-	
-		for i := 1; i <= count; i {
-			value, err := DecodeRESP(byteStream)
-			if err != nil {
-				return Value{}, err
-			}
-	
-			array = append(array, value)
-		}
-	
-		return Value{
-			typ:   Array,
-			array: array,
-		}, nil
-	}
-	
-	func readUntilCRLF(byteStream *bufio.Reader) ([]byte, error) {
-		readBytes := []byte{}
-	
-		for {
-			b, err := byteStream.ReadBytes('\n')
-			if err != nil {
-				return nil, err
-			}
-	
-			readBytes = append(readBytes, b...)
-			if len(readBytes) >= 2 && readBytes[len(readBytes)-2] == '\r' {
-				break
-			}
-		}
-	
-		return readBytes[:len(readBytes)-2], nil
-	}
+
+	return readBytes[:len(readBytes)-2], nil
+}
